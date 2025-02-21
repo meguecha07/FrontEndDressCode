3f19da2511a6fb3436a50690a0d4470c9fb40073
Melissa Guerra <melissaguerrachamorro@gmail.com>
2025-02-20 19:25:52 -0500

    Header

--- src/components/website/layout/WebsiteHeader/WebsiteHeader.jsx
+++ src/components/website/layout/WebsiteHeader/WebsiteHeader.jsx
@@ -1,7 +1,27 @@
-export const WebsiteHeader = () => {
-  return (
-    <div>WebsiteHeader</div>
-  )
-}
+import { Link } from 'react-router-dom';
+import styles from './WebsiteHeader.module.css';
+import logoImage from '../../../../assets/logoDresscode.webp';
 
-export default WebsiteHeader
+const WebsiteHeader = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        {/* Bloque izquierdo: logotipo y lema */}
+        <div className={styles.leftBlock}>
+          <Link to="/" className={styles.logoLink}>
+          <img src={logoImage} alt="" />
+            <h1 className={styles.logo}>Dresscode</h1>
+            <span className={styles.slogan}>Tu moda, tu estilo</span>
+          </Link>
+        </div>
+        {/* Bloque derecho: botones de "Crear cuenta" e "Iniciar sesión" */}
+        <div className={styles.rightBlock}>
+          <button className={styles.button}>Crear cuenta</button>
+          <button className={styles.button}>Iniciar sesión</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default WebsiteHeader;
