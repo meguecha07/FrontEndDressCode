2e9c05c284bd9b70b7490e66724765980fbdff74
Melissa Guerra <melissaguerrachamorro@gmail.com>
2025-03-06 01:25:34 -0500

    cambio en el hover de los categoryButton

--- src/components/website/ui/CategoryList/CategoryList.jsx
+++ src/components/website/ui/CategoryList/CategoryList.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/prop-types */
 import styles from './CategoryList.module.css';
 
 
 const CategoryList = ({categories}) => {
   return (
-    <div className={styles.categoryList}>
+    <div className={styles.categoryList} id="efect">
       {categories.map((category, index) => (
         <button key={index} className={styles.categoryButton}>
           <img src={category.imageUrl} alt={category.name} className={styles.categoryImage} />
