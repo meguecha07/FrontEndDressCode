e82f47a074a95ad0c821da5daa5356256602c264
Melissa Guerra <melissaguerrachamorro@gmail.com>
2025-03-07 09:57:12 -0500

    caracteristicas icons

--- src/components/website/ui/ProductDetail/ProductDetail.jsx
+++ src/components/website/ui/ProductDetail/ProductDetail.jsx
@@ -1,4 +1,6 @@
 import styles from "./ProductDetail.module.css";
+import icon from "../../../../assets/vestido.png";
+import icon1 from "../../../../assets/codigo-de-vestimenta.png";
 
 const ProductDetail = ({ product }) => {
     // Mock data para los atributos que no están en el objeto `product`
@@ -31,43 +33,43 @@ const ProductDetail = ({ product }) => {
                 <div className={styles.list}>
                     {/* Mostrar todos los atributos */}
                     <div className={styles.attribute}>
-                        <img src="/icon-tipo-prenda.svg" alt="Tipo de prenda" />
+                        <img src={icon} alt="vestido" />
                         <p>
                             <strong>Tipo de prenda:</strong> {productDetails.tipoDePrenda}
                         </p>
                     </div>
                     <div className={styles.attribute}>
-                        <img src="/icon-talla.svg" alt="Talla" />
+                        <i class="fa-solid fa-arrow-up-1-9"></i>
                         <p>
                             <strong>Talla:</strong> {productDetails.talla}
                         </p>
                     </div>
                     <div className={styles.attribute}>
-                        <img src="/icon-color.svg" alt="Color" />
+                        <i class="fa-solid fa-shirt"></i>
                         <p>
                             <strong>Color:</strong> {productDetails.color}
                         </p>
                     </div>
                     <div className={styles.attribute}>
-                        <img src="/icon-material.svg" alt="Material" />
+                        <i class="fa-brands fa-cotton-bureau"></i>
                         <p>
                             <strong>Material:</strong> {productDetails.material}
                         </p>
                     </div>
                     <div className={styles.attribute}>
-                        <img src="/icon-estado.svg" alt="Estado" />
+                        <i class="fa-brands fa-neos"></i>
                         <p>
                             <strong>Estado:</strong> {productDetails.estado}
                         </p>
                     </div>
                     <div className={styles.attribute}>
-                        <img src="/icon-estilo.svg" alt="Estilo" />
+                        <i class="fa-solid fa-vest-patches"></i>
                         <p>
                             <strong>Estilo:</strong> {productDetails.estilo}
                         </p>
                     </div>
                     <div className={styles.attribute}>
-                        <img src="/icon-temporada.svg" alt="Temporada" />
+                        <i class="fa-solid fa-sun"></i>
                         <p>
                             <strong>Temporada:</strong> {productDetails.temporada}
                         </p>
@@ -75,37 +77,37 @@ const ProductDetail = ({ product }) => {
                 </div>
                 <div className={styles.list}>
                     <div className={styles.attribute}>
-                        <img src="/icon-ocasion.svg" alt="Ocasión" />
+                        <img src={icon1} alt="ocasión" />
                         <p>
                             <strong>Ocasión:</strong> {productDetails.ocasion}
                         </p>
                     </div>
                     <div className={styles.attribute}>
-                        <img src="/icon-marca.svg" alt="Marca/Diseñador" />
+                        <i class="fa-regular fa-chess-queen"></i>
                         <p>
                             <strong>Marca/Diseñador:</strong> {productDetails.marcaDiseñador}
                         </p>
                     </div>
                     <div className={styles.attribute}>
-                        <img src="/icon-patron.svg" alt="Patrón" />
+                        <i class="fa-solid fa-shapes"></i>
                         <p>
                             <strong>Patrón:</strong> {productDetails.patron}
                         </p>
                     </div>
                     <div className={styles.attribute}>
-                        <img src="/icon-largo.svg" alt="Largo" />
+                        <i class="fa-solid fa-arrows-turn-right"></i>
                         <p>
                             <strong>Largo:</strong> {productDetails.largo}
                         </p>
                     </div>
                     <div className={styles.attribute}>
-                        <img src="/icon-ajuste.svg" alt="Ajuste" />
+                        <i class="fa-solid fa-arrows-left-right-to-line"></i>   
                         <p>
                             <strong>Ajuste:</strong> {productDetails.ajuste}
                         </p>
                     </div>
                     <div className={styles.attribute}>
-                        <img src="/icon-genero.svg" alt="Género" />
+                        <i class="fa-regular fa-circle-check"></i>
                         <p>
                             <strong>Género:</strong> {productDetails.genero}
                         </p>
